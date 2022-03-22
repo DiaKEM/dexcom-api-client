d45b920cdde067fc2a3d35723f95bdb38a357148
Selcuk Kekec <khskekec@gmail.com>
2022-03-22 21:13:30 +0100

    chore: some env fixes

--- lib/client.d.ts
+++ lib/client.d.ts
@@ -2,19 +2,28 @@ import { AxiosRequestConfig } from 'axios';
 import schedule from 'node-schedule';
 import { CGMDataType } from './utils';
 declare type DexcomApiClientType = {
-    username: string;
-    password: string;
-    server: 'US' | 'EU';
-    clientOpts?: AxiosRequestConfig;
+  username: string;
+  password: string;
+  server: 'US' | 'EU';
+  clientOpts?: AxiosRequestConfig;
 };
-export declare const DexcomApiClient: ({ username, password, server, clientOpts, }: DexcomApiClientType) => {
-    login: () => Promise<void>;
-    read: (minutesAgo?: number, count?: number) => Promise<CGMDataType[]>;
-    readLast: () => Promise<CGMDataType[]>;
-    observe: ({ maxAttempts, delay, listener, }: {
-        maxAttempts?: number | undefined;
-        delay?: number | undefined;
-        listener: (data: CGMDataType) => void;
-    }) => Promise<schedule.Job>;
+export declare const DexcomApiClient: ({
+  username,
+  password,
+  server,
+  clientOpts,
+}: DexcomApiClientType) => {
+  login: () => Promise<void>;
+  read: (minutesAgo?: number, count?: number) => Promise<CGMDataType[]>;
+  readLast: () => Promise<CGMDataType[]>;
+  observe: ({
+    maxAttempts,
+    delay,
+    listener,
+  }: {
+    maxAttempts?: number | undefined;
+    delay?: number | undefined;
+    listener: (data: CGMDataType) => void;
+  }) => Promise<schedule.Job>;
 };
 export {};
